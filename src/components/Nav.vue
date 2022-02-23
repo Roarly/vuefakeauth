5726fbdfc2f52317c9f12a8d8ae1e190f10f2be9
Abel Quintanilla <abelqui15@gmail.com>
2022-02-23 15:27:08 -0600

    Menu Login Logic

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,3 +1,10 @@
+<script>
+import useAuth from "../composables/useAuth";
+const {isAuthenticated, logout} = useAuth();
+</script>
+
+
+
 <template>
 <div class="text-blue-500 bg-gray-600">
     <div class="container flex items-center justify-between py-4 mx-auto">
@@ -5,10 +12,12 @@
         </h1>
         <nav>
             <ul class="flex ">
-                <router-link to="/"><li class="px-5 py-4 bg-gray-300 rounded-l-2xl hover:cursor-pointer hover:bg-slate-500 hover:text-white">Home</li></router-link>
+                <router-link to="/"><li class="px-5 py-4 bg-gray-300 hover:cursor-pointer hover:bg-slate-500 hover:text-white">Home</li></router-link>
                 <router-link to="/about"><li class="px-5 py-4 bg-gray-400 hover:cursor-pointer hover:bg-slate-500 hover:text-white">About</li></router-link>
-                <router-link to="/login"><li class="px-5 py-4 bg-gray-300 hover:cursor-pointer hover:bg-slate-500 hover:text-white">Login</li></router-link>
-                <router-link to="/secret"><li class="px-5 py-4 bg-gray-400 rounded-r-2xl hover:cursor-pointer hover:bg-slate-500 hover:text-white">Secret</li></router-link>
+                <router-link v-if="!isAuthenticated" to="/login"><li class="px-5 py-4 bg-gray-300 hover:cursor-pointer hover:bg-slate-500 hover:text-white">Login</li></router-link>
+                
+                <router-link v-else to="/secret"><li class="px-5 py-4 bg-gray-400 hover:cursor-pointer hover:bg-slate-500 hover:text-white">Secret</li></router-link>
+                <button v-else @click="logout"><li class="px-5 py-4 bg-gray-300 hover:cursor-pointer rounded-r-2xl hover:bg-slate-500 hover:text-white">Logout</li></button>
             </ul>
         </nav>
     </div>
