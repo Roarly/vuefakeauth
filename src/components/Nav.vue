ae8747ce9804e601155d1ed48b520c1c21a4f548
Abel Quintanilla <abelqui15@gmail.com>
2022-02-23 14:57:39 -0600

    Added secret page

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -7,8 +7,8 @@
             <ul class="flex ">
                 <router-link to="/"><li class="px-5 py-4 bg-gray-300 rounded-l-2xl hover:cursor-pointer hover:bg-slate-500 hover:text-white">Home</li></router-link>
                 <router-link to="/about"><li class="px-5 py-4 bg-gray-400 hover:cursor-pointer hover:bg-slate-500 hover:text-white">About</li></router-link>
-                <router-link to="/login"><li class="px-5 py-4 bg-gray-300 rounded-r-2xl hover:cursor-pointer hover:bg-slate-500 hover:text-white">Login</li></router-link>
-                
+                <router-link to="/login"><li class="px-5 py-4 bg-gray-300 hover:cursor-pointer hover:bg-slate-500 hover:text-white">Login</li></router-link>
+                <router-link to="/secret"><li class="px-5 py-4 bg-gray-400 rounded-r-2xl hover:cursor-pointer hover:bg-slate-500 hover:text-white">Secret</li></router-link>
             </ul>
         </nav>
     </div>
